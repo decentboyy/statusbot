49e74e8e94fb31f31610eecaea4f6812ac15a110
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-22 14:18:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,35 +28,36 @@ OWNER_ID = int(os.getenv("OWNER_ID"))
 bot.start()
 
 
-async def main():
-    print("Status Checker Bot Started")
-    async with app:
-        while True:
-            TEXT = "This is the live bot status of all Bots 🤖"
-            for bots in BOT_LIST:
-                ok = await app.get_users(f"@{bots}")
-                try:
-                    await app.send_message(bots, "/start")
-                    await asyncio.sleep(2)
-                    messages = app.get_chat_history(bots, limit=1)
-                    async for x in messages:
-                        msg = x.text
-                    if msg == "/start":
-                        TEXT += f"\n\n**🤖-[{ok.first_name}](tg://openmessage?user_id={ok.id}): OFFLINE** 💀"
-                        await bot.send_message(OWNER_ID, f'Alert {ok.first_name} is offline 💀')
-                        await app.read_chat_history(bots)
-                    else:
-                        TEXT += f"\n\n**🤖-[{ok.first_name}](tg://openmessage?user_id={ok.id}): {msg}**"
-                        await app.read_chat_history(bots)
-                except FloodWait as e:
-                    await asyncio.sleep(e.value)
-            time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
-            date = time.strftime("%d %b %Y")
-            time = time.strftime("%I:%M: %p")
-            TEXT += f"\n\n--Last checked on--: \n{date}\n{time} ({TIME_ZONE})\n\n**Refreshes Automatically After Every 15 Min.**"
-            await bot.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID,
-                                        TEXT)
-            await asyncio.sleep(900)
+import psutil
+import time
+from _ import start_time, Client # replace _ where you declare the start_time, Client
+from pyrogram import filters 
+from pyrogram.types import Message
+
+# TeamUltroid/Ultroid
+def time_formatter(milliseconds):
+    minutes, seconds = divmod(int(milliseconds / 1000), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    tmp = (((str(weeks) + "w:") if weeks else "") +
+           ((str(days) + "d:") if days else "") +
+           ((str(hours) + "h:") if hours else "") +
+           ((str(minutes) + "m:") if minutes else "") +
+           ((str(seconds) + "s") if seconds else ""))
+    if not tmp:
+        return "0s"
+    if tmp.endswith(":"):
+        return tmp[:-1]
+    return tmp
+
+
+@Client.on_message(filters.command('statusbot') & filters.private)
+async def activevc(_, message: Message):
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    cpu = psutil.cpu_percent()
+    TEXT = f"UPTIME: {uptime} | CPU: {cpu}%"
+    await message.reply(TEXT)
 
 
 bot.run(main())
