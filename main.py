7dcb8c96689f91bfc6f8f66767a30f9752477e67
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-24 17:44:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 app = Client(api_id=int(os.getenv('API_ID')),
              api_hash=os.getenv("6647d9d827e9e1fdb810c1b27cef423b"),
-             session_string=os.getenv("BQGwSY0AqK0LySN4sa_bNvk_UexxgRsibItUMRYKznLn9qMCN6S1Ih9gRW3fprrxN_XctoW8rTxI2Es5uQh_F9-Uk5ow9yKvLfX-ylU1hkM598mTRqIr-tMpQqd0n9AfUkjE8pSCtcxeFZaYIsMaPqDUdSIPJFtJie0RnsQJYcnOOkXT63up0WlPNprlLRecSViy2cAJS8GqX2z89u825Lad3HI-cTO7_jryGfBfTCNmVUiFivysys5dFglXaEzXCvqcbQWnYnCvoSTm2ru3MwQYLWJDr_1lDt2a3JeDA207KBtep0crQ9Q09HJrF7zv-_xnMdoyhyuE_9QBSVkHdvbcKlyTqAAAAAFAylHOAA"))
+             session_name=os.getenv("BQGwSY0AqK0LySN4sa_bNvk_UexxgRsibItUMRYKznLn9qMCN6S1Ih9gRW3fprrxN_XctoW8rTxI2Es5uQh_F9-Uk5ow9yKvLfX-ylU1hkM598mTRqIr-tMpQqd0n9AfUkjE8pSCtcxeFZaYIsMaPqDUdSIPJFtJie0RnsQJYcnOOkXT63up0WlPNprlLRecSViy2cAJS8GqX2z89u825Lad3HI-cTO7_jryGfBfTCNmVUiFivysys5dFglXaEzXCvqcbQWnYnCvoSTm2ru3MwQYLWJDr_1lDt2a3JeDA207KBtep0crQ9Q09HJrF7zv-_xnMdoyhyuE_9QBSVkHdvbcKlyTqAAAAAFAylHOAA"))
 
 bot = Client(api_id=int(os.getenv("28330381")),
              api_hash=os.getenv("6647d9d827e9e1fdb810c1b27cef423b"),
