1b389106458b1a9515e20d4cfb6a50d0905249c9
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-05-24 17:04:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import datetime
 import pytz
+import psutil
 
 from dotenv import load_dotenv
 from pyrogram import Client
@@ -25,7 +26,15 @@ MESSAGE_ID = int(os.getenv("MESSAGE_ID"))
 TIME_ZONE = os.getenv("TIME_ZONE")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 
-bot.start()
+
+async def get_uptime():
+    uptime = datetime.datetime.now() - datetime.datetime.fromtimestamp(psutil.boot_time())
+    return str(uptime)
+
+
+async def get_cpu_usage():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    return f"{cpu_usage}%"
 
 
 async def main():
@@ -33,29 +42,29 @@ async def main():
     async with app:
         while True:
             TEXT = "This is the live bot status of all Bots 🤖"
-            for bots in BOT_LIST:
-                ok = await app.get_users(f"@{bots}")
+            for bot_username in BOT_LIST:
+                bot_user = await app.get_users(bot_username)
                 try:
-                    await app.send_message(bots, "/start")
+                    await app.send_message(bot_username, "/statusbot")
                     await asyncio.sleep(2)
-                    messages = app.get_chat_history(bots, limit=1)
-                    async for x in messages:
-                        msg = x.text
-                    if msg == "/start":
-                        TEXT += f"\n\n**🤖-[{ok.first_name}](tg://openmessage?user_id={ok.id}): OFFLINE** 💀"
-                        await bot.send_message(OWNER_ID, f'Alert {ok.first_name} is offline 💀')
-                        await app.read_chat_history(bots)
-                    else:
-                        TEXT += f"\n\n**🤖-[{ok.first_name}](tg://openmessage?user_id={ok.id}): {msg}**"
-                        await app.read_chat_history(bots)
+                    messages = await app.get_chat_history(bot_username, limit=1)
+                    async for message in messages:
+                        if message.text == "/statusbot":
+                            TEXT += f"\n\n**🤖-[{bot_user.first_name}](tg://openmessage?user_id={bot_user.id}): OFFLINE** 💀"
+                            await bot.send_message(OWNER_ID, f"Alert {bot_user.first_name} is offline 💀")
+                        else:
+                            uptime = await get_uptime()
+                            cpu_usage = await get_cpu_usage()
+                            TEXT += f"\n\n**🤖-[{bot_user.first_name}](tg://openmessage?user_id={bot_user.id}): {message.text}**"
+                            TEXT += f"\nUptime: {uptime}"
+                            TEXT += f"\nCPU Usage: {cpu_usage}"
                 except FloodWait as e:
-                    await asyncio.sleep(e.value)
-            time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+                    await asyncio.sleep(e.x)
+            time = datetime.datetime.now(pytz.timezone(TIME_ZONE))
             date = time.strftime("%d %b %Y")
             time = time.strftime("%I:%M: %p")
             TEXT += f"\n\n--Last checked on--: \n{date}\n{time} ({TIME_ZONE})\n\n**Refreshes Automatically After Every 15 Min.**"
-            await bot.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID,
-                                        TEXT)
+            await bot.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, TEXT)
             await asyncio.sleep(900)
 
 
